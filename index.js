d120873b2b5567c84fc9f005836ec314362aad55
fruhzen <tyabkank56@gmail.com>
2025-02-16 14:47:36 +0530

    smooth scroll and intuitive UI

---
+++ index.js
@@ -0,0 +1,10 @@
+        //Smooth scrolling
+        const links = document.querySelectorAll("nav a[href^='#']");
+        links.forEach(link => {
+            link.addEventListener("click", function(event) {
+                event.preventDefault();
+                const targetId = this.getAttribute("href").substring(1);
+                const targetElement = document.getElementById(targetId);
+                window.scrollTo({ top: targetElement.offsetTop - 60, behavior: "smooth" });
+            });
+        });
